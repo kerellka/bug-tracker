222d6446586bb87970367942f797049355b12c79
Kirill Yakovlev <kerellka1@mail.ru>
2020-11-14 15:37:06 +0300

    Added User profile view

--- src/main/java/org/suai/tracker_test/controller/UserController.java
+++ src/main/java/org/suai/tracker_test/controller/UserController.java
@@ -1,7 +1,28 @@
 package org.suai.tracker_test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.suai.tracker_test.model.User;
+import org.suai.tracker_test.service.UserService;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(@CurrentSecurityContext(expression = "authentication.name") String username, Model model) {
+        User user = userService.findByLogin(username);
+        model.addAttribute("user", user);
+        model.addAttribute("projects", user.getProjects());
+        return "users/profile";
+    }
 }
